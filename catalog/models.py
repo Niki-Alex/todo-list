2b70620ab830bbd7dca0f6b57bda9da3d023c80d
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-08-07 10:38:08 +0300

    Create model Tag and Task. Migrate them

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    completed = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["completed", "-created"]
+
+    def __str__(self):
+        return (
+            f"Сontent: {self.content}\n"
+            f"Created {self.created}\n"
+            f"Deadline: {self.deadline}\n"
+            f"Tags: {self.tags}"
+        )
